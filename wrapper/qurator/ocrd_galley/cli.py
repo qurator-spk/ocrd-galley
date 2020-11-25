04d16d93d05819a7998fd4b6aefc02f52d69ddf0
Gerber, Mike <mike.gerber@sbb.spk-berlin.de>
2020-11-25 19:46:50 +0100

    ⬆️ ocrd_calamari 1.0.0 (plus changes for legacy ocrd_calmari)

--- wrapper/qurator/ocrd_galley/cli.py
+++ wrapper/qurator/ocrd_galley/cli.py
@@ -14,7 +14,7 @@ sub_images = {
         "ocrd-sbb-binarize": "sbb_binarization",
         "ocrd-sbb-textline-detector": "sbb_textline_detector",
         "ocrd-calamari-recognize": "ocrd_calamari",
-        "ocrd-calamari-recognize-feat-update-calamari1": "ocrd_calamari-feat-update-calamari1",
+        "ocrd-calamari-recognize03": "ocrd_calamari03",
         "ocrd-tesserocr-segment-region": "ocrd_tesserocr",
         "ocrd-tesserocr-segment-line": "ocrd_tesserocr",
         "ocrd-tesserocr-recognize": "ocrd_tesserocr",
