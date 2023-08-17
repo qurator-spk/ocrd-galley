ad8ea8c3ef45cfd09ce14f51764d94993316557d
Gerber, Mike <mike.gerber@sbb.spk-berlin.de>
2023-08-17 19:03:10 +0200

    ✔ Add test-ocrd_wrap.sh

---
+++ test-ocrd_wrap.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -ex
+
+test_id=`basename $0`
+cd `mktemp -d /tmp/$test_id-XXXXX`
+
+# Prepare test workspace
+wget https://qurator-data.de/examples/actevedef_718448162.first-page+binarization+segmentation.zip
+unzip actevedef_718448162.first-page+binarization+segmentation.zip
+cd actevedef_718448162.first-page+binarization+segmentation
+
+# Run tests
+ocrd-skimage-binarize -I OCR-D-IMG -O TEST-OCRD-SKIMAGE-BINARIZE
